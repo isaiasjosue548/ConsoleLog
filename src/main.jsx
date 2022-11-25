76e5a2dce70de0007b203b679031861ad50aad50
isaiasjosue548 <isaiasq56@gmail.com>
2022-11-25 14:07:33 +0100

    First Commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { LogisticApp } from './components/LogisticApp'
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <LogisticApp />
+  </React.StrictMode>
+)
